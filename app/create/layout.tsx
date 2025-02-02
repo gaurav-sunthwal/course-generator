dd8d58e70589f16b93c78755935bf49257a9fc7f
Gaurav Naresh Sunthwal <70788155+codewithgaurav252005@users.noreply.github.com>
2025-02-02 14:55:27 +0530

    error fix

--- app/create/layout.tsx
+++ app/create/layout.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import Header from '../_components/Header'
+import React from "react";
+import Header from "../_components/Header";
 
-export default function layout({children}:{children:React.ReactNode}) {
+export default function layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
-        <Header params='create' />
+      <Header params="create" />
       {children}
     </div>
-  )
+  );
 }
